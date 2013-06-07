243cee0842ffc2b4f8892edee42f946d3992a561
Darren Neimke <darren.neimke@gmail.com>
2013-06-08 07:36:28 +0930

    moved BacklogController into a strict scoped block

--- scripts/BacklogController.js
+++ scripts/BacklogController.js
@@ -1,14 +1,18 @@
-function BacklogController($scope, phaseFilter) {
-    $scope.backlogItems = phaseFilter([
-        {title: 'Sugar Data Migration', phase: 1},
-        {title: 'Daily ITSS Admin', phase: 3},
-        {title: 'UI Testing Process - MTM', phase: 3},
-        {title: 'OVS CROWD Integration', phase: 4},
-        {title: 'SugarCRM WebForms Integration', phase: 4},
-        {title: 'Mothership Test Cases - Primary Calculators', phase: 4},
-        {title: 'FELS Decommissioning - OriginatorDb Timeline', phase: 4},
-        {title: 'FELS Decommissioning - DTS Action Plan', phase: 4},
-        {title: 'Database Strategy Development', phase: 4},
-        {title: 'UI Testing Process - Requirements and Test Cases', phase: 5}
-    ]);
-}
+(function () {
+    'use strict';
+    
+    function BacklogController($scope, phaseFilter) {
+        $scope.backlogItems = phaseFilter([
+            {title: 'Sugar Data Migration', phase: 1},
+            {title: 'Daily ITSS Admin', phase: 3},
+            {title: 'UI Testing Process - MTM', phase: 3},
+            {title: 'OVS CROWD Integration', phase: 4},
+            {title: 'SugarCRM WebForms Integration', phase: 4},
+            {title: 'Mothership Test Cases - Primary Calculators', phase: 4},
+            {title: 'FELS Decommissioning - OriginatorDb Timeline', phase: 4},
+            {title: 'FELS Decommissioning - DTS Action Plan', phase: 4},
+            {title: 'Database Strategy Development', phase: 4},
+            {title: 'UI Testing Process - Requirements and Test Cases', phase: 5}
+        ]);
+    }
+}());
