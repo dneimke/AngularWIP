6adaff0de4165f68e941ab89448c64435c838779
Darren Neimke <darren.neimke@gmail.com>
2013-06-10 08:47:43 +0930

    Added action buttons to the tray of each card

--- scripts/BacklogController.js
+++ scripts/BacklogController.js
@@ -1,17 +1,87 @@
 function BacklogController($scope, phaseFilter) {
-    
     'use strict';
     
-    $scope.backlogItems = phaseFilter([
-        {title: 'Sugar Data Migration', phase: 1},
-        {title: 'Daily ITSS Admin', phase: 3},
-        {title: 'UI Testing Process - MTM', phase: 3},
-        {title: 'OVS CROWD Integration', phase: 4},
-        {title: 'SugarCRM WebForms Integration', phase: 4},
-        {title: 'Mothership Test Cases - Primary Calculators', phase: 4},
-        {title: 'FELS Decommissioning - OriginatorDb Timeline', phase: 4},
-        {title: 'FELS Decommissioning - DTS Action Plan', phase: 4},
-        {title: 'Database Strategy Development', phase: 4},
-        {title: 'UI Testing Process - Requirements and Test Cases', phase: 5}
-    ]);
+    var jobs = [{
+        title: 'Sugar Data Migration',
+        description: 'Vestibulum sit amet enim euismod, sodales tellus et, aliquam velit. Donec mattis dolor sagittis sapien posuere fermentum et id leo. Integer imperdiet pulvinar nulla quis fermentum. Sed et posuere nisi. Aenean enim nulla, facilisis sit amet consectetur nec, porta eu nisl. Nullam justo nisl, interdum eget elementum ut, convallis et erat. Fusce posuere mi id erat sodales suscipit.',
+        assignedTo: 'Darren Neimke',
+        dueDate: '21-Jun-13',
+        phase: 1
+    },
+        {
+            title: 'Daily ITSS Admin',
+            description: 'Vestibulum sit amet enim euismod, sodales tellus et, aliquam velit. Donec mattis dolor sagittis sapien posuere fermentum et id leo. Integer imperdiet pulvinar nulla quis fermentum. Sed et posuere nisi. Aenean enim nulla, facilisis sit amet consectetur nec, porta eu nisl. Nullam justo nisl, interdum eget elementum ut, convallis et erat. Fusce posuere mi id erat sodales suscipit.',
+            assignedTo: 'Darren Neimke',
+            dueDate: '21-Jun-13',
+            phase: 3
+        },
+        {
+            title: 'UI Testing Process - MTM',
+            description: 'Vestibulum sit amet enim euismod, sodales tellus et, aliquam velit. Donec mattis dolor sagittis sapien posuere fermentum et id leo. Integer imperdiet pulvinar nulla quis fermentum. Sed et posuere nisi. Aenean enim nulla, facilisis sit amet consectetur nec, porta eu nisl. Nullam justo nisl, interdum eget elementum ut, convallis et erat. Fusce posuere mi id erat sodales suscipit.',
+            assignedTo: 'Darren Neimke',
+            dueDate: '21-Jun-13',
+            phase: 3
+        },
+        {
+            title: 'OVS CROWD Integration',
+            description: 'Vestibulum sit amet enim euismod, sodales tellus et, aliquam velit. Donec mattis dolor sagittis sapien posuere fermentum et id leo. Integer imperdiet pulvinar nulla quis fermentum. Sed et posuere nisi. Aenean enim nulla, facilisis sit amet consectetur nec, porta eu nisl. Nullam justo nisl, interdum eget elementum ut, convallis et erat. Fusce posuere mi id erat sodales suscipit.',
+            assignedTo: 'Darren Neimke',
+            dueDate: '21-Jun-13',
+            phase: 4
+        },
+        {
+            title: 'SugarCRM WebForms Integration',
+            description: 'Vestibulum sit amet enim euismod, sodales tellus et, aliquam velit. Donec mattis dolor sagittis sapien posuere fermentum et id leo. Integer imperdiet pulvinar nulla quis fermentum. Sed et posuere nisi. Aenean enim nulla, facilisis sit amet consectetur nec, porta eu nisl. Nullam justo nisl, interdum eget elementum ut, convallis et erat. Fusce posuere mi id erat sodales suscipit.',
+            assignedTo: 'Darren Neimke',
+            dueDate: '21-Jun-13',
+            phase: 4
+        },
+        {
+            title: 'Mothership Test Cases - Primary Calculators',
+            description: 'Vestibulum sit amet enim euismod, sodales tellus et, aliquam velit. Donec mattis dolor sagittis sapien posuere fermentum et id leo. Integer imperdiet pulvinar nulla quis fermentum. Sed et posuere nisi. Aenean enim nulla, facilisis sit amet consectetur nec, porta eu nisl. Nullam justo nisl, interdum eget elementum ut, convallis et erat. Fusce posuere mi id erat sodales suscipit.',
+            assignedTo: 'Darren Neimke',
+            dueDate: '21-Jun-13',
+            phase: 4
+        },
+        {
+            title: 'FELS Decommissioning - OriginatorDb Timeline',
+            description: 'Vestibulum sit amet enim euismod, sodales tellus et, aliquam velit. Donec mattis dolor sagittis sapien posuere fermentum et id leo. Integer imperdiet pulvinar nulla quis fermentum. Sed et posuere nisi. Aenean enim nulla, facilisis sit amet consectetur nec, porta eu nisl. Nullam justo nisl, interdum eget elementum ut, convallis et erat. Fusce posuere mi id erat sodales suscipit.',
+            assignedTo: 'Darren Neimke',
+            dueDate: '21-Jun-13',
+            phase: 4
+        },
+        {
+            title: 'FELS Decommissioning - DTS Action Plan',
+            description: 'Vestibulum sit amet enim euismod, sodales tellus et, aliquam velit. Donec mattis dolor sagittis sapien posuere fermentum et id leo. Integer imperdiet pulvinar nulla quis fermentum. Sed et posuere nisi. Aenean enim nulla, facilisis sit amet consectetur nec, porta eu nisl. Nullam justo nisl, interdum eget elementum ut, convallis et erat. Fusce posuere mi id erat sodales suscipit.',
+            assignedTo: 'Darren Neimke',
+            dueDate: '21-Jun-13',
+            phase: 4
+        },
+        {
+            title: 'Database Strategy Development',
+            description: 'Vestibulum sit amet enim euismod, sodales tellus et, aliquam velit. Donec mattis dolor sagittis sapien posuere fermentum et id leo. Integer imperdiet pulvinar nulla quis fermentum. Sed et posuere nisi. Aenean enim nulla, facilisis sit amet consectetur nec, porta eu nisl. Nullam justo nisl, interdum eget elementum ut, convallis et erat. Fusce posuere mi id erat sodales suscipit.',
+            assignedTo: 'Darren Neimke',
+            dueDate: '21-Jun-13',
+            phase: 4
+        },
+        {
+            title: 'UI Testing Process - Requirements and Test Cases',
+            description: 'Vestibulum sit amet enim euismod, sodales tellus et, aliquam velit. Donec mattis dolor sagittis sapien posuere fermentum et id leo. Integer imperdiet pulvinar nulla quis fermentum. Sed et posuere nisi. Aenean enim nulla, facilisis sit amet consectetur nec, porta eu nisl. Nullam justo nisl, interdum eget elementum ut, convallis et erat. Fusce posuere mi id erat sodales suscipit.',
+            assignedTo: 'Darren Neimke',
+            dueDate: '21-Jun-13',
+            phase: 5
+        }];
+    
+    $scope.backlogItems = phaseFilter(jobs);
+    
+    
+    $scope.moveLeft = function (job) {
+        job.phase -= 1;
+        $scope.backlogItems = phaseFilter(jobs);
+    };
+    
+    $scope.moveRight = function (job) {
+        job.phase += 1;
+        $scope.backlogItems = phaseFilter(jobs);
+    };
 }
